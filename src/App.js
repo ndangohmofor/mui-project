c0b20b7ec01d9df849e1d6c0e609c70426715433
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-22 07:19:27 -0500

    Add components in App js and wrapped them around a Stack display component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material";
 import Send from "@mui/icons-material/Send";
 import Settings from "@mui/icons-material/Settings";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <Box>
+      {/* Navbar */}
+      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
