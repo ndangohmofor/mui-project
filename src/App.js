677c48bb5ec6e41d701b71e688059ddf27528ebe
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-22 07:09:55 -0500

    Added components directory with navbar, sidebar and rightbar

--- src/App.js
+++ src/App.js
@@ -4,46 +4,8 @@ import { styled } from "@mui/material";
 import Send from "@mui/icons-material/Send";
 import Settings from "@mui/icons-material/Settings";
 
-const StyledBox = styled(Box)({
-  margin: 3,
-});
-
-const BlueButton = styled(Button)({
-  backgroundColor: "lightblue",
-  color: "#000",
-  margin: 5,
-  "&:hover": {
-    backgroundColor: "skyblue",
-  },
-  "&:disabled": {
-    backgroundColor: "gray",
-  },
-});
-
 function App() {
-  return (
-    <>
-      <StyledBox>
-        <BlueButton variant="contained" startIcon={<Send />}>
-          Send
-        </BlueButton>
-      </StyledBox>
-      <StyledBox>
-        <BlueButton
-          variant="contained"
-          color="secondary"
-          startIcon={<Settings />}
-        >
-          Setting
-        </BlueButton>
-      </StyledBox>
-      <StyledBox>
-        <BlueButton variant="contained" startIcon={<Settings />} disabled>
-          Disabled
-        </BlueButton>
-      </StyledBox>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
