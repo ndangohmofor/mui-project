d3a86fa16c0f257c3f817beb484ee06fefa723ae
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-22 06:40:47 -0500

    Creating personal styles for various components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material";
+import Send from "@mui/icons-material/Send";
+import Settings from "@mui/icons-material/Settings";
+
+const StyledBox = styled(Box)({
+  margin: 3,
+});
+
+const BlueButton = styled(Button)({
+  backgroundColor: "lightblue",
+  color: "#000",
+  margin: 5,
+  "&:hover": {
+    backgroundColor: "skyblue",
+  },
+  "&:disabled": {
+    backgroundColor: "gray",
+  },
+});
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <StyledBox>
+        <BlueButton variant="contained" startIcon={<Send />}>
+          Send
+        </BlueButton>
+      </StyledBox>
+      <StyledBox>
+        <BlueButton variant="contained" startIcon={<Settings />}>
+          Setting
+        </BlueButton>
+      </StyledBox>
+      <StyledBox>
+        <BlueButton variant="contained" startIcon={<Settings />} disabled>
+          Disabled
+        </BlueButton>
+      </StyledBox>
+    </>
+  );
 }
 
 export default App;
