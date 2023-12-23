104ea1e360e724aeb8cc3720856fd9674b3c0bad
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-23 09:07:36 -0500

    Added a theme provide and setup a useState to manage the theme. Passed the state hook and variable down to the side bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material";
+import { ThemeProvider, createTheme, styled } from "@mui/material";
 import Send from "@mui/icons-material/Send";
 import Settings from "@mui/icons-material/Settings";
 import Sidebar from "./components/Sidebar";
@@ -9,18 +9,29 @@ import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import Navbar from "./components/Navbar";
 import AddPost from "./components/AddPost";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <Navbar />
-      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-      <AddPost />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <AddPost />
+      </Box>
+    </ThemeProvider>
   );
 }
 
