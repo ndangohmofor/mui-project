67bdf24c7fd8664501fb3dd91253ac34a73bc862
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-22 19:23:27 -0500

    Update Addpost functionality to provide text area for adding a post along with the emojis and option for photo and video

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Fab from "@mui/material/Fab";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import AddIcon from "@mui/icons-material/Add";
+import { styled } from "@mui/material";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import ImageIcon from "@mui/icons-material/Image";
+import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
 
 const AddPost = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Tooltip
+        onClick={(e) => setOpen(true)}
         title="Add Post"
         sx={{
           position: "fixed",
@@ -21,6 +50,50 @@ const AddPost = () => {
           </Fab>
         </IconButton>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor={"white"} p={3} borderRadius={5}>
+          <Typography variant="h6" color={"grey"} textAlign={"center"}>
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&w=600"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography>John Doe</Typography>
+          </UserBox>
+          <TextField
+            id="standard-multiline-static"
+            sx={{ width: "100%" }}
+            multiline
+            rows={4}
+            variant="standard"
+            placeholder="What's on your mind?"
+          ></TextField>
+          <Stack direction={"row"} gap={1} mt={2} mb={3}>
+            <EmojiEmotionsIcon color="primary" />
+            <ImageIcon color="secondary" />
+            <VideoCameraBackIcon color="success" />
+            <PersonAddIcon color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRangeIcon />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </>
   );
 };
